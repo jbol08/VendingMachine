cd1a23d3e8d1cc99f3c373124cc04ebc0f77d02b
Jose Bolanos <bolanosjjr@gmail.com>
2021-03-26 21:37:18 -0500

    took out code not being used

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -2,23 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import vendingMachineImg from "./vendingmachine.png";
 import './App.css';
-// import Soda from './soda';
-// import Chips from './chips';
-// import Cookies from './cookies';
 
 
 function VendingMachine() {
-    // const [page, setPage] = useState("home");
-  
-    // const goToPage = newPage => {
-    //   setPage(newPage);
-    // }
-  
-    // const showPage = () => {
-    //   if (page === "soda") return <Soda />;
-    //   if (page === "chips") return <Chips />
-    //   if (page === "cookies") return <Cookies/>
-    // }
+   
   
     return (        
     <div
